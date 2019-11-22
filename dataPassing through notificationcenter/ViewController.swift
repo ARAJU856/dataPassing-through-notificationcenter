c9390db38cba36809da8af3227761eb41b1788d7
A RAJU <avularaju9866@achiever-115.local>
2019-11-22 16:03:22 +0530

    Initial Commit

---
+++ dataPassing through notificationcenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dataPassing through notificationcenter
+//
+//  Created by A RAJU on 11/22/19.
+//  Copyright © 2019 A RAJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
